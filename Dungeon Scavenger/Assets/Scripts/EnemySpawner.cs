dc2f1ea17a81ef6cbdde276469c571c210d0bb05
spjalf <hossain.alaf@elev.ga.lbs.se>
2025-01-08 16:25:50 +0100

    Jag har gjort spawner till enemy

--- Dungeon Scavenger/Assets/Scripts/EnemySpawner.cs
+++ Dungeon Scavenger/Assets/Scripts/EnemySpawner.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public GameObject Enemy;
     void Start()
     {
-        
+        float random = Random.Range(-4, 3);
+        GameObject Enemyclone = Instantiate(Enemy, transform.position = new Vector2(15, random) ,Quaternion.identity);
     }
 
     // Update is called once per frame
