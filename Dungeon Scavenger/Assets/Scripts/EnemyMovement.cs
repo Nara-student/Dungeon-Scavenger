2cb12c489c72468ef65d9f339412ba7d90aa18ef
Nara-student <nara.chen@elev.ga.lbs.se>
2025-01-13 12:46:48 +0100

    fixade goblin movement animation

--- Dungeon Scavenger/Assets/Scripts/EnemyMovement.cs
+++ Dungeon Scavenger/Assets/Scripts/EnemyMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.XR;
@@ -18,6 +19,7 @@ public class EnemyMovementCombat : MonoBehaviour
     private Collider2D triggerCollider; //Collider to trigger distance;
     private Animator anim;
 
+
     private void Awake()
     {
         instance = this;
@@ -27,7 +29,7 @@ public class EnemyMovementCombat : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         triggerCollider = GetComponent<Collider2D>();
-
+        anim = GetComponent<Animator>();
     }
 
 
@@ -42,25 +44,64 @@ public class EnemyMovementCombat : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        isInTriggerDistance = true;
+        if(collision.gameObject.name == "Player")
+        {
+            isInTriggerDistance = true;
+        }
     }
 
     void follow()
     {
         //Keeps from interlapping with target (Player)
         distanceStop = Vector2.Distance(transform.position, target.transform.position);
-        anim.Play("Idle Animation");
+        //anim.Play("Idle Animation");
 
         if (distanceStop >= 1.3)
         {
+            Vector2 dir = target.transform.position - transform.position;
+
+            //Direction decider
+            if (Mathf.Abs(dir.x) - Mathf.Abs(dir.y) < 0.5f)
+            {
+                dir.x = 0;
+            }
+            else if (Mathf.Abs(dir.x) < Mathf.Abs(dir.y))
+            {
+                dir.x = 0;
+            }
+            else if (Mathf.Abs(dir.x) > Mathf.Abs(dir.y))
+            {
+                dir.y = 0;
+            }
+            
+
+            rb.velocity = dir.normalized * speed;
+
+            if(rb.velocity.y == -speed)
+            {
+                anim.Play("GoblinForwardWalk");
+            }
+            if(rb.velocity.y == speed)
+            {
+                anim.Play("GoblinBackwardsWalk");
+            }
+            if(rb.velocity.x == -speed)
+            {
+                anim.Play("GoblinLeftWalk");
+            }
+            if (rb.velocity.x == speed)
+            {
+                anim.Play("GoblinRightWalk");
+            }
+
             //Makes the Enemy follow target (Player)
-            transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
-            anim.Play("Run Animation");
+            //transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+            //anim.Play("Run Animation");
         }
     }
 
     public void attackAnimation()
     {
-        anim.Play("Attack Animation"); //Might need work for while damage
+        //anim.Play("Attack Animation"); //Might need work for while damage
     }
 }
