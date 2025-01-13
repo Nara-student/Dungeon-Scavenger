9006a8492654fb25825d926746521c19b03f75a5
spjalf <hossain.alaf@elev.ga.lbs.se>
2025-01-13 15:26:30 +0100

    jag gjorde parkour map och kod

--- Dungeon Scavenger/Assets/Scripts/ParkourMovement.cs
+++ Dungeon Scavenger/Assets/Scripts/ParkourMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -7,8 +8,10 @@ using UnityEngine;
 public class ParkourMovement : MonoBehaviour
 {
     public int Speed;
-
+    int jumpCount = 0;
     Rigidbody2D Rb;
+    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +21,8 @@ public class ParkourMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        
         if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             Rb.velocity = new Vector2(-Speed, Rb.velocity.y);
@@ -28,10 +33,37 @@ public class ParkourMovement : MonoBehaviour
             Rb.velocity = new Vector2(Speed, Rb.velocity.y);
         }
 
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && jumpCount < 2)
         {
-            Rb.velocity = new Vector2(Rb.velocity.x, 3);
+            jumpCount++;
+            if(jumpCount < 2)
+            {
+              
+                Rb.velocity = new Vector2(Rb.velocity.x, 5);
+            }
+
+
+          
         }
 
+     
+
     }
+
+    private void FixedUpdate()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position - (transform.up *0.75f), -Vector2.up);
+
+        if (hit)
+        {
+            float distance = Mathf.Abs(hit.point.y - transform.position.y);
+            if(distance < 1)
+            {
+                jumpCount = 0;
+            }
+        }
+        
+
+    }
+
 }
